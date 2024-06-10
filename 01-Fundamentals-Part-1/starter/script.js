419e44c8cbd42aea498739b02cc65d164082cef1
BungeeDragon <skillmanesther@gmail.com>
2024-06-10 13:18:55 +0100

    Learnt illegal, legal and conventional js var names

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,10 @@
 let js = 'amazing';
-if (js == 'amazing') alert('Javascript is FUN!');
-
-40 + 8 + 23 - 10;
-let math = 1;
 console.log(40 + 8 + 23 - 10);
-console.log(math);
+
+let jonas_matilda = "JM"; //Legal
+let $function = 27; //Legal
+
+let Person = 'jonas'; //against convention - use for OOP
+let person = 'jonas';
+
+let PI = 3.1325; //VS recognises this
