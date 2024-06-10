405a8a2f31819a18a4601e50711f0a50a22a180e
BungeeDragon <skillmanesther@gmail.com>
2024-06-10 13:21:02 +0100

    typeof console log

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,8 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
 
-let jonas_matilda = "JM"; //Legal
-let $function = 27; //Legal
+let isIsland = false;
+let language;
 
-let Person = 'jonas'; //against convention - use for OOP
-let person = 'jonas';
-
-let PI = 3.1325; //VS recognises this
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
