93b929ee878b167ba25e726dd429d2ec711f03a5
BungeeDragon <skillmanesther@gmail.com>
2024-06-10 13:32:20 +0100

    Strings and template literals: Fail

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,27 @@
-let language = 'portuguese';
-const country = 'Portugal';
-const continent = 'Europe';
-const isIsland = false;
-let isIsland = true;
+let population = 11;
+const country = "Portugal";
+const continent = "Europe";
+const language = "Spanish";
+
+console.log(population / 2);
+
+population++;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description1 =
+    country +
+    ' is in ' +
+    continent +
+    ', and its ' +
+    population +
+    ' million people speak ' +
+    language;
+
+console.log(description1);
+
+const description = '${country} is in ${continent}, and its ${population} million people speak ${language}';
+
+console.log(description);
