27f5252c0b10a2767c2ac470fdece093431a3624
BungeeDragon <skillmanesther@gmail.com>
2024-09-11 18:01:30 +0100

    Used template literals + changed challenges.html title

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,23 +1,40 @@
-let age = 30;
-age = 31; //Mutated variable
+// let age = 30;
+// age = 31; //Mutated variable
+//
+// //const birthYear = 1991; //Cannot be mutated
+//
+// // const name; //Uncaught SyntaxError: Missing initializer in const declaration
+//
+// // Try to have as little mutations in your code as possible - they create bugs
+//
+// //var job = 'programmer'; //Old way of defining variables prior to ES6
+// //job = 'teacher';
+//
+// lastName = 'Smith';
+//
+// //Arithmetic
+// multiplication = 2 ** 3; // 2 to the power of 3
+// x = 3; // 3
+// x += 4; // 3 + 4 = 7
+// x -= 4; // 7 - 4 = 3
+// x++; // 3 + 1 = 4
+//
+//
+// console.log(x); // Doesn't create a variable on the scope by default - JS creates a property on the global object (not a good idea)
 
-const birthYear = 1991; //Cannot be mutated
+//Strings
 
-// const name; //Uncaught SyntaxError: Missing initializer in const declaration
+const firstName = "Esther";
+const job = "Student";
+const birthYear = 2004
+const year = 2024
 
-// Try to have as little mutations in your code as possible - they create bugs
+// Not fun string:
+const esther = "I'm " + firstName + ", a " + (year - birthYear) + ' year old ' + job + "!";
 
-var job = 'programmer'; //Old way of defining variables prior to ES6 
-job = 'teacher';
+console.log(esther);
 
-lastName = 'Smith';
+//Template literals: much more fun
+const newEsther = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
 
-//Arithmetic
-multiplication = 2 ** 3; // 2 to the power of 3
-x = 3; // 3
-x += 4; // 3 + 4 = 7
-x -= 4; // 7 - 4 = 3
-x++; // 3 + 1 = 4
-
-
-console.log(x); // Doesn't create a variable on the scope by default - JS creates a property on the global object (not a good idea)
+console.log(newEsther);
