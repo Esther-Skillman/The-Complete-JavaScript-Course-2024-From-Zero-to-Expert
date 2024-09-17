b70e0211e3be052321962e63737c87fbb4e30f60
BungeeDragon <skillmanesther@gmail.com>
2024-09-17 15:00:46 +0100

    (17) Finished template literals - mutiple lines without /n/

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -34,7 +34,7 @@ const esther = "I'm " + firstName + ", a " + (year - birthYear) + ' year old ' +
 
 console.log(esther);
 
-//Template literals: much more fun
+//TEMPLATE LITERALS (`): much more fun
 const newEsther = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
 
 console.log(newEsther);
@@ -42,3 +42,15 @@ console.log(newEsther);
 console.log("Which speech marks should I use?")
 
 console.log(`Use backticks! It's standard and you don't have to decide :)`)
+//Incase you need to add a variable aswell, you don't need to go back and change the regular quotes to ` !
+
+console.log('String with \n\
+multiple \n\
+lines \n\ ');
+//Before ESXi (Definition: https://www.youtube.com/watch?v=-Hltydu9PXk&ab_channel=RobWillis)
+
+console.log(`String also with
+multiple
+lines`);
+//After ESXi - a lot cleaner
+
