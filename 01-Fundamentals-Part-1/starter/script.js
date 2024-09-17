04efeb6d246af0e066dcb86950cb6c17a452f5c3
BungeeDragon <skillmanesther@gmail.com>
2024-09-17 15:05:32 +0100

    Added else statment with template literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -24,33 +24,42 @@
 
 //Strings
 
-const firstName = "Esther";
-const job = "Student";
-const birthYear = 2004
-const year = 2024
+// const firstName = "Esther";
+// const job = "Student";
+// const birthYear = 2004
+// const year = 2024
+//
+// // Not fun string:
+// const esther = "I'm " + firstName + ", a " + (year - birthYear) + ' year old ' + job + "!";
+//
+// console.log(esther);
+//
+// //TEMPLATE LITERALS (`): much more fun
+// const newEsther = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+//
+// console.log(newEsther);
+//
+// console.log("Which speech marks should I use?")
+//
+// console.log(`Use backticks! It's standard and you don't have to decide :)`)
+// //Incase you need to add a variable aswell, you don't need to go back and change the regular quotes to ` !
+//
+// console.log('String with \n\
+// multiple \n\
+// lines \n\ ');
+// //Before ESXi (Definition: https://www.youtube.com/watch?v=-Hltydu9PXk&ab_channel=RobWillis)
+//
+// console.log(`String also with
+// multiple
+// lines`);
+// //After ESXi - a lot cleaner
 
-// Not fun string:
-const esther = "I'm " + firstName + ", a " + (year - birthYear) + ' year old ' + job + "!";
+const age = 15;
+const isOldEnough = age >= 18;
 
-console.log(esther);
-
-//TEMPLATE LITERALS (`): much more fun
-const newEsther = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
-
-console.log(newEsther);
-
-console.log("Which speech marks should I use?")
-
-console.log(`Use backticks! It's standard and you don't have to decide :)`)
-//Incase you need to add a variable aswell, you don't need to go back and change the regular quotes to ` !
-
-console.log('String with \n\
-multiple \n\
-lines \n\ ');
-//Before ESXi (Definition: https://www.youtube.com/watch?v=-Hltydu9PXk&ab_channel=RobWillis)
-
-console.log(`String also with
-multiple
-lines`);
-//After ESXi - a lot cleaner
-
+if (isOldEnough){
+    console.log(`Sarah can start to drive`)
+}
+else{
+    console.log(`Sarah is only ${age}! she can't drive yet. She needs to wait ${18 - age} more years until she can`)
+}
