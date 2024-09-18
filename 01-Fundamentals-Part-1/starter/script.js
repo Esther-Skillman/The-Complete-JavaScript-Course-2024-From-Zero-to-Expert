f5f53045bf1eb4ee21858728471fc3bde3a00995
BungeeDragon <skillmanesther@gmail.com>
2024-09-18 12:26:51 +0100

    (18) Finished Taking Decisions: if/else Statements - added static variable birth year and new 'century' if-else code block

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,7 +54,9 @@
 // lines`);
 // //After ESXi - a lot cleaner
 
-const age = 15;
+const birthYear = 2004;
+const currentYear = 2024;
+const age = currentYear - birthYear;
 const isOldEnough = age >= 18;
 
 if (isOldEnough){
@@ -64,3 +66,13 @@ else{
     const yearsLeft = 18 - age;
     console.log(`Sarah is only ${age}! she can't drive yet. She needs to wait ${yearsLeft} more years until she can`)
 }
+// called an 'if' 'else' control structure
+
+let century; //needs to be defined outside if and else code blocks to be accessible outside of them
+
+if (birthYear <= 2000){
+    century = 20;
+} else {
+    century = 21
+}
+console.log(`Birth Century: ${century}`)
