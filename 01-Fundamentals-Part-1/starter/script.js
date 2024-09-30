35d3da842fee77c9d9f2a1cb18ab88836ba6bff4
BungeeDragon <skillmanesther@gmail.com>
2024-09-30 22:00:19 +0100

    Type Conversion and Coercion - Implemented conversion examples

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,25 +54,33 @@
 // lines`);
 // //After ESXi - a lot cleaner
 
-const birthYear = 2004;
-const currentYear = 2024;
-const age = currentYear - birthYear;
-const isOldEnough = age >= 18;
+// const birthYear = 2004;
+// const currentYear = 2024;
+// const age = currentYear - birthYear;
+// const isOldEnough = age >= 18;
+//
+// if (isOldEnough){
+//     console.log(`Sarah can start to drive`)
+// }
+// else{
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is only ${age}! she can't drive yet. She needs to wait ${yearsLeft} more years until she can`)
+// }
+// // called an 'if' 'else' control structure
+//
+// let century; //needs to be defined outside if and else code blocks to be accessible outside of them
+//
+// if (birthYear <= 2000){
+//     century = 20;
+// } else {
+//     century = 21
+// }
+// console.log(`Birth Century: ${century}`)
 
-if (isOldEnough){
-    console.log(`Sarah can start to drive`)
-}
-else{
-    const yearsLeft = 18 - age;
-    console.log(`Sarah is only ${age}! she can't drive yet. She needs to wait ${yearsLeft} more years until she can`)
-}
-// called an 'if' 'else' control structure
+// TYPE CONVERSION AND COERCION
 
-let century; //needs to be defined outside if and else code blocks to be accessible outside of them
-
-if (birthYear <= 2000){
-    century = 20;
-} else {
-    century = 21
-}
-console.log(`Birth Century: ${century}`)
+// Conversion:
+const inputYear = '2004'
+console.log(inputYear + 18) //Prints 200418
+console.log(Number(inputYear) + 18) // Prints 2022
+console.log(Number('Esther')) // Prints NaN (not a number)
